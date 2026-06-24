f59fe2a94feb977e7375c35ba90e9c9d6e4fc072
NaCk3rZ! <nahue.dintal@gmail.com>
2026-06-24 13:50:43 -0300

    agregado de constructor, y getters básicos en cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,36 @@
 public class Cliente{
+  private int idCliente;
+  private String nombre;
+  private String apellido;
+  private String dni;
+  private int telefono;
+
+  public Cliente(int idCliente, String nombre, String apellido, String dni, int telefono) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.telefono = telefono;
+  }
+  
+  public int getIdCliente() {
+    return idCliente;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public String getDni() {
+    return dni;
+  }
+
+  public int getTelefono() {
+    return telefono;
+  }
+
 
 }
