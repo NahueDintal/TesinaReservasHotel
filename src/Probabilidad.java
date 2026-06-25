87bac9302703d50b8af0886c6489b7fb7787fdc0
NaCk3rZ! <nahue.dintal@gmail.com>
2026-06-24 21:11:04 -0300

    agregado de probabilidad para matematicas

---
+++ src/Probabilidad.java
@@ -0,0 +1,11 @@
+public class Probabilidad {
+  private int totalReservas;
+  private int totalCanceladas;
+
+  public Probabilidad(int totalReservas, int totalCanceladas) {
+    this.totalReservas = totalReservas;
+    this.totalCanceladas = totalCanceladas;
+  }
+
+
+}
