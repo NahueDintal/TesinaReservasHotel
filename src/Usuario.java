815b0df38ac695ff9ecbe3abccbb1c056b0da1ce
NaCk3rZ! <nahue.dintal@gmail.com>
2026-06-25 21:50:44 -0300

    agregado de clase usuario

---
+++ src/Usuario.java
@@ -0,0 +1,36 @@
+public class Usuario {
+  private int idUsuario;
+  private String nombre;
+  private String clave;
+  private boolean habilitado;
+  private String permisos; // escala de permisos, usuario, admin, o persimisos a seleccionar??
+  private String area;
+
+  public Usuario(int idUsuario, String nombre, Strin clave, boolean habilitado, String permisos, String area) {
+    this.nombre = nombre;
+    this.clave = clave;
+    this.habilitado = true;
+    this.permisos = permisos;
+    this.area = area;
+  }
+
+  public int getIdUsuario() {
+    return idUsuario;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public boolean getHabilitado() {
+    return habilitado;
+  }
+
+  public String getPersimisos() {
+    return persimisos;
+  }
+
+  public String getArea() {
+    return area;
+  }
+}
