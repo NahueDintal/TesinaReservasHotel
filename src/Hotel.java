ef2c797018f679180dffd5c9373b4cc303b941a3
NaCk3rZ! <nahue.dintal@gmail.com>
2026-06-26 17:12:32 -0300

    agregado de tareas en hotel.java

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,3 +1,8 @@
 public class Hotel{
 
+  // funcion para buscar reservas por nombre en db
+  // funcion para cancelar reserva
+  // funcion para crear reserva
+  // funcion para modificar reserva
+  
 }
