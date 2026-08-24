22a9c1870e5488eca461015a4d8a803bedd412ca
NaCk3rZ! <nahue.dintal@gmail.com>
2026-08-24 12:47:29 -0300

    agregado de tree correspondiente en main

---
+++ src/main/java/models/Probability.java
@@ -0,0 +1,20 @@
+package models;
+
+public class Probability {
+  private int totalReservation;
+  private int totalCancelled;
+
+  public Probability(int totalReservation, int totalCancelled) {
+    this.totalReservation = totalReservation;
+    this.totalCancelled = totalCancelled;
+  }
+
+  public double probabilityCancelled() {
+    return (double) totalCancelled / totalReservation;
+  }
+
+  // cancelación de temporada baja
+  // tendría que hacer una consulta con los meses que se denominan como temp baja
+  // cancelación en temporada alta
+  // idem al anterior
+}
---
+++ src/main/java/models/Reservation.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Reservation {
+  private Client client;
+  private Room room;
+  private LocalDate initDate;
+  private LocalDate endDate;
+  private double total;
+
+  public Reservation(Client client, Room room, LocalDate initDate, LocalDate endDate) {
+    if (client != null) {
+      this.client = client;
+    }
+    this.room = room;
+    this.initDate = initDate; // aca tendría que haber una de error por la fecha
+    this.endDate = endDate;
+    // this.total = dias * precio por noche * servicios * consumos en bar
+  }
+
+  public LocalDate getInitDate() {
+    return initDate;
+  }
+
+  public LocalDate getEndDate() {
+    return endDate;
+  }
+
+  public Room getRoom() {
+    return room;
+  }
+
+  public Client getClient() {
+    return client;
+  }
+
+  public double getTotal() {
+    return total;
+  }
+}
