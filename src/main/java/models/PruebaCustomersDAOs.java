12fecc16fd3a7a6690c191cc4be1c402809dd4b2
Ignacio Guzman <ignacioguzman608.2@gmail.com>
2026-08-24 13:47:31 -0300

    Actualizaciones para clases relacionadas a customer || Estilos actualizados || Nuevo controlador para Customer || Logo del hotel añadido en resources || Nuevo CustomerView.fxml

--- src/main/java/models/PruebaCustomersDAOs.java
+++ src/main/java/models/PruebaCustomersDAOs.java
@@ -8,7 +8,7 @@ public class PruebaCustomersDAOs {
         try {
             // Probar DocumentType
             DocumentTypeDAO docDAO = new DocumentTypeDAO();
-            Map<Integer, String> tipos = docDAO.listarTodos();
+            Map<Integer, String> tipos = docDAO.listAll();
             System.out.println("=== TIPOS DE DOCUMENTO ===");
             for (Map.Entry<Integer, String> entry : tipos.entrySet()) {
                 System.out.println(entry.getKey() + " - " + entry.getValue());
@@ -16,7 +16,7 @@ public class PruebaCustomersDAOs {
 
             // Probar Country
             CountryDAO paisDAO = new CountryDAO();
-            Map<Integer, String> paises = paisDAO.listarTodos();
+            Map<Integer, String> paises = paisDAO.listAll();
             System.out.println("\n=== PAÍSES ===");
             for (Map.Entry<Integer, String> entry : paises.entrySet()) {
                 System.out.println(entry.getKey() + " - " + entry.getValue());
@@ -24,7 +24,7 @@ public class PruebaCustomersDAOs {
 
             // Probar CustomerStatus
             CustomerStatusDAO statusDAO = new CustomerStatusDAO();
-            Map<Integer, String> estados = statusDAO.listarTodos();
+            Map<Integer, String> estados = statusDAO.listAll();
             System.out.println("\n=== ESTADOS ===");
             for (Map.Entry<Integer, String> entry : estados.entrySet()) {
                 System.out.println(entry.getKey() + " - " + entry.getValue());
@@ -32,7 +32,7 @@ public class PruebaCustomersDAOs {
 
             // Probar CustomerOrigin
             CustomerOriginDAO origenDAO = new CustomerOriginDAO();
-            Map<Integer, String> origenes = origenDAO.listarTodos();
+            Map<Integer, String> origenes = origenDAO.listAll();
             System.out.println("\n=== ORÍGENES ===");
             for (Map.Entry<Integer, String> entry : origenes.entrySet()) {
                 System.out.println(entry.getKey() + " - " + entry.getValue());
