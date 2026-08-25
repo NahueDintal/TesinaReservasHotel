1f7a04f091a518f947417e0701bfeacf06af2c6e
NaCk3rZ! <nahue.dintal@gmail.com>
2026-08-25 11:24:46 -0300

    agregado de los metodos que faltaban, creo que falta completar el delete

--- src/main/java/models/RoomDAO.java
+++ src/main/java/models/RoomDAO.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 
 public class RoomDAO {
 
-  public Room seachById(int id) {
-    String sql = "SELECT id, precio_base, tipo, tiene_balcon, tiene_jacuzzi FROM habitacion WHERE id = ?";
+  public Room seachByNumber(int number) {
+    String sql = "SELECT * FROM room WHERE number = ?";
     try (Connection conn = ConexionDB.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-      pstmt.setInt(1, id);
+      pstmt.setInt(1, number);
       try (ResultSet rs = pstmt.executeQuery()) {
         if (rs.next()) {
           return mapRoom(rs);
         }
       }
     } catch (SQLException e) {
-      throw new RuntimeException("Error al obtener la habitación por id.");
+      throw new RuntimeException("Error al obtener la habitación por número.", e);
     }
     return null;
   }
@@ -30,28 +30,62 @@ public class RoomDAO {
       while (rs.next()) {
         rooms.add(mapRoom(rs));
       }
-    } catch (SQLException e ){
-      throw new RuntimeException("Error al listar las habitaciones.");
+    } catch (SQLException e) {
+      throw new RuntimeException("Error al listar las habitaciones.", e);
     }
     return rooms;
   }
 
-//  public insert() {
-//    String sql = "";
-//    try (Connection conn = ConexionDB.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//      pstmt.setInt(1, id);
-//      try (ResultSet rs = pstmt.executeQuery()) {
-//        if (rs.next()) {
-//          return mapRoom(rs);
-//        }
-//      }
-//    } catch (SQLException e) {
-//      throw new RuntimeException("Error al insertar la habitación.");
-//    }
-//    return null;
-//  }
+  public void insert(Room room) {
+    String sql = "INSERT INTO room (number, floor, type, capacity, view, available, out_of_service, features, price, description)"
+        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    try (Connection conn = ConexionDB.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setInt(1, room.getNumber());
+      pstmt.setInt(2, room.getFloor());
+      pstmt.setString(3, room.getType());
+      pstmt.setInt(4, room.getCapacity());
+      pstmt.setString(5, room.view());
+      pstmt.setBoolean(6, room.getIsAvailable());
+      pstmt.setBoolean(7, room.getOutOfService());
+      pstmt.setString(8, room.getFeatures());
+      pstmt.setDouble(9, room.getPrice());
+      pstmt.setString(10, room.getDescription());
+      pstmt.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException("Error al insertar habitacion.", e);
+    }
+  }
 
-//  public upDate
+  public void update(Room room) {
+    String sql = "UPDATE room SET floor = ?, type = ?, capacity = ?, view = ?, available = ?,"
+        + "out_of_service = ?, features = ?, price = ?, description = ?, WHERE number = ?";
+    try (Connection conn = ConexionDB.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setInt(1, room.getFloor());
+      pstmt.setString(2, room.getType());
+      pstmt.setInt(3, room.getCapacity());
+      pstmt.setString(4, room.getView());
+      pstmt.setBoolean(5, room.getIsAvailable());
+      pstmt.setBoolean(6, room.getOutOfService());
+      pstmt.setString(7, room.getFeatures());
+      pstmt.setDouble(8, room.getPrice());
+      pstmt.setString(9, room.getDescription());
+      pstmt.setInt(10, room.getNumber());
+      pstmt.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException("Error al actualizar habitación.", e);
+    }
+  }
+
+  public void delete(int number) {
+    String sql = "DELETE FROM room WHERE number = ?";
+    try (Connection conn = ConexionDB.getConnection();
+          PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setInt(1, room.setNu)
+    }
+
+  }
 
   private Room mapRoom(ResultSet rs) throws SQLException {
     String numberStr = rs.getString("number");
