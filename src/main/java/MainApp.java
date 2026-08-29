2d30ff067a1739a4cf061be506787e8922acbe6e
NaCk3rZ! <nahue.dintal@gmail.com>
2026-08-29 11:02:38 -0300

    intentando resolver conflictos en mainapp

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 public class MainApp extends Application {
 
   @Override
-<<<<<<< HEAD
   public void start(Stage primaryStage) throws Exception {
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Dashboard.fxml"));
     BorderPane root = loader.load();
@@ -16,18 +15,16 @@ public class MainApp extends Application {
     primaryStage.setTitle("Sistema de Reservas - Dashboard");
     primaryStage.setScene(scene);
     primaryStage.show();
-=======
+  }
+
   public void start(Stage stage) throws Exception {
-    // ===== CARGA DEL FXML (tu código original) =====
     FXMLLoader loader = new FXMLLoader(
-            getClass().getResource("/views/CustomerView.fxml")
-    );
+        getClass().getResource("/views/CustomerView.fxml"));
 
     Scene scene = new Scene(loader.load());
     stage.setTitle("Gestión de Clientes - Hotel");
     stage.setScene(scene);
     stage.show();
->>>>>>> b30a62b (ScriptsSQL Actualizados || mainApp limpiado || archivos relacionados a Customer Actualizados)
   }
 
   public static void main(String[] args) {
